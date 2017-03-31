c9abc7e798580546bd69b4703171f1c655ff0c1b
fgoncalvesaf <fgoncalvesaf@gmail.com>
2017-03-31 16:28:39 -0300

    continuando as alteracoes na view

--- app/models/installment.rb
+++ app/models/installment.rb
@@ -1,5 +1,5 @@
 class Installment < ApplicationRecord
   belongs_to :sale
   validates :value, presence: true, numericality: true
-  validates :date, presence: true
+  validates :payment_date, presence: true
 end
